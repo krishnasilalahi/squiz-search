9afbba7826bbc6217328e73ae55b2e64b278cc33
Krishna Silalahi <krishna.silalahi@gmail.com>
2023-03-03 11:44:22 +1000

    refactored logger and minor refactoring

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Squiz\PhpCodeExam\Logger;
+use Squiz\PhpCodeExam\Searcher;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
 $path = $request->getPathInfo();
+$searcher = new Searcher();
+$logger = new Logger();
 
-logMsg(
+$logger->logMsg(
     sprintf('Got request %s', json_encode($request)),
     'request'
 );
@@ -20,7 +24,10 @@ try {
             $response = new JsonResponse(['data' => false], Response::HTTP_BAD_REQUEST);
         } else {
             $term = $request->query->get('term');
-            $response = new JsonResponse(['data' => (new \Squiz\PhpCodeExam\Searcher())->execute($term, $type = 'content')], Response::HTTP_OK);
+            $response = new JsonResponse(
+                ['data' => (new Searcher())->execute($term, $type = 'content')],
+                Response::HTTP_OK
+            );
         }
     }
     else if (preg_match('/tags/', $path) !== 0) {
@@ -28,7 +35,10 @@ try {
             $response = new JsonResponse(['data' => false], Response::HTTP_BAD_REQUEST);
         } else {
             $term = $request->query->get('term');
-            $response = new JsonResponse(['data' => (new \Squiz\PhpCodeExam\Searcher())->execute($term, 'tags')], Response::HTTP_OK);
+            $response = new JsonResponse(
+                ['data' => (new Searcher())->execute($term, 'tags')],
+                Response::HTTP_OK
+            );
         }
     } else if (preg_match('/pages/', $path) !== 0) {
         $paths = explode('/', $path);
@@ -36,43 +46,28 @@ try {
         if ($id == null) {
             $response = new JsonResponse(['data' => false], Response::HTTP_BAD_REQUEST);
         } else {
-            $result = (new \Squiz\PhpCodeExam\Searcher())->getPageById($id);
-            $response = new JsonResponse(['data' => $result], $result ? Response::HTTP_PARTIAL_CONTENT : Response::HTTP_NOT_FOUND);
+            $result = (new Searcher())->getPageById($id);
+            $response = new JsonResponse(['data' => $result],
+                $result ? Response::HTTP_PARTIAL_CONTENT : Response::HTTP_NOT_FOUND);
         }
     } else {
-        // print all
-        $searcher = new \Squiz\PhpCodeExam\Searcher();
+        // print all (is it needed ??)
         $data = $searcher->allData;
 
-        $null = NULL;
-        $response = empty($data) ? new Response(NULL, Response::HTTP_NO_CONTENT) : new JsonResponse(['data' => $data], Response::HTTP_ACCEPTED);
+        $response = empty($data) ?
+            new Response(NULL, Response::HTTP_NO_CONTENT) :
+            new JsonResponse(['data' => $data], Response::HTTP_ACCEPTED);
     }
 
     $response->send();
-    logMsg(
+    $logger->logMsg(
         sprintf('Sent response %s', $response->getContent()),
         'response'
     );
 } catch (Exception $ex) {
-    new JsonResponse(['exception' => $ex->getMessage()], Response::HTTP_BAD_REQUEST);
-    logMsg(
+    $response = new JsonResponse(['exception' => $ex->getMessage()], Response::HTTP_BAD_REQUEST);
+    $response->send();
+    $logger->logMsg(
         sprintf('%s: %s', $ex->getCode(), $ex->getMessage())
     );
-}
-
-function logMsg($message, $type = 'error')
-{
-    $logger = new \Squiz\PhpCodeExam\Logger();
-
-    switch ($type) {
-        case 'error':
-            $file = __DIR__ . '/logs/error.log';
-        case 'request':
-            $file = __DIR__ . '/logs/request.log';
-        case 'response':
-            $file = __DIR__ . '/logs/response.log';
-        default:
-            $file = __DIR__ . '/logs/log.log';
-    }
-    $logger->log($message, $file);
 }
--- src/Logger.php
+++ src/Logger.php
@@ -4,15 +4,22 @@ namespace Squiz\PhpCodeExam;
 
 class Logger
 {
-    public function log($message, string $fileName)
+    /**
+     * @param $message
+     * @param string $type
+     */
+    function logMsg($message, string $type = 'error')
     {
-        if (is_dir(__DIR__ . '/../logs')) {
-            $file = fopen($fileName,'a+');
-            fwrite($file, $message . "\n", 128);
-            fclose($file);
-            return;
+        if (!is_dir(__DIR__ . '/../logs')) {
+            mkdir(__DIR__ . '/../logs');
         }
 
-        mkdir(__DIR__ . '/../logs');
+        $file = match ($type) {
+            'error' => __DIR__ . '/../logs/error.log',
+            'request' => __DIR__ . '/../logs/request.log',
+            'response' => __DIR__ . '/../logs/response.log',
+            default => __DIR__ . '/../logs/log.log'
+        };
+        error_log($message.PHP_EOL, 3, $file);
     }
 }
